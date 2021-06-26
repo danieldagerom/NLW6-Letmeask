63a430e8e25f2731c2648fa332027d8c6ce99d2a
danieldagerom <danielprimo9@gmail.com>
2021-06-26 14:02:36 -0300

    adicionando melhora em funções e contexto

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -6,12 +6,13 @@ import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 
 import { Button } from '../components/Button'
+//import { useAuth } from '../hooks/useAuth'
 
 import '../styles/auth.scss'
 
 
 export function NewRoom(){
-    //const { user } = useContext(AuthContext);
+    //const { user } = useAuth();
 
     return(
         <div id="page-auth">
