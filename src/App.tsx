835f5099cff13d52eff9253f1a85888a723c53c9
danieldagerom <danielprimo9@gmail.com>
2021-07-08 17:30:39 -0300

    adicionando pagina de admin e estilos de questoes

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,9 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import { Home } from "./pages/Home"; 
 import { NewRoom } from "./pages/NewRoom"; 
-
 import { AuthContextProvider } from './contexts/AuthContexts'
 import { Room } from "./pages/Room";
-
+import { AdminRoom } from "./pages/AdminRoom";
 
 function App() {
   return (
@@ -15,6 +14,8 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+
+          <Route path="/admin/rooms/:id" component={AdminRoom} />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
